1db537e0ffbd18a724e3416e5ef23677f332f9a0
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2019-10-03 03:06:21 +0000

    - LSIS 옴론 PLC 변환기   * Test ladder graph builder

--- PLCConvertor/FormPLCConverter.cs
+++ PLCConvertor/FormPLCConverter.cs
@@ -37,69 +37,9 @@ namespace PLCConvertor
             //}
 
         }
-
-
-
-        string input1 = @"LD A
-AND B
-OR C
-OUT D
-";
-
-        string input2 = @"LD A
-AND B
-LD C
-ORLD
-OUT D
-";
-
-        string input3 = @"LD A
-OUT TR0
-AND B
-OUT O1
-LD TR0
-AND C
-OUT O2
-";
-
-
-        string input4 = @"LD 0.00
-OUT TR0
-AND 0.01
-OUT 110.00
-LD TR0
-AND 110.00
-OUT 102.10
-";
-
-        string input5 = @"LD 0.00
-LD 0.01
-OUT TR0
-AND 0.02
-ORLD
-AND 0.03
-OUT 102.11
-LD TR0
-AND 0.04
-OUT 102.12
-";
-
-        string input6 = @"LD 0.00
-LD 0.01
-OUT TR0
-AND 0.02
-ORLD
-AND 0.03
-OUT 102.11
-LD TR0
-AND 0.04
-OUT 102.12
-";
-
-
         void TestConversion()
         {
-            var inputs = input4.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var inputs = FormInputSelector.Inputs[0].Input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
             var rung = Rung.CreateRung(inputs);
             var graph = rung.GraphViz();
             var _pictureBox = new PictureBox() { Image = graph, Dock = DockStyle.Fill };
@@ -112,7 +52,7 @@ OUT 102.12
         Form _lastEmbeddedForm;
         private void BarButtonItemTestParse_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
         {
-            var formILs = new FormSimpleEditor() { Title = "Enter Instruction Lists", Contents=input2 };
+            var formILs = new FormInputSelector();
             if (formILs.ShowDialog() == DialogResult.OK)
             {
                 if (_lastEmbeddedForm != null)
