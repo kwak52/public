2b885ff38c3758e820d3c00ef3a503bead392e26
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2019-11-13 01:04:03 +0000

    - LSIS 옴론 PLC 변환기   * 정리 작업 중.

--- Dsu.PLCConvertor.Common/Internal/IL/IL.Omron.cs
+++ Dsu.PLCConvertor.Common/Internal/IL/IL.Omron.cs
@@ -13,51 +13,51 @@ namespace Dsu.PLCConvertor.Common.Internal
         /// </summary>
         static Dictionary<Mnemonic, List<ILCommand>> _dicOmron = new Dictionary<Mnemonic, List<ILCommand>>()
         {
-            [Mnemonic.LOAD] = _toList(1, "LD"),
-            [Mnemonic.LOADNOT] = _toList(1, "LDNOT"),
-            [Mnemonic.AND] = _toList(1, "AND"),
-            [Mnemonic.ANDNOT] = _toList(1, "ANDNOT"),
-            [Mnemonic.ANDLD] = _toList(1, "ANDLD"),
-            [Mnemonic.OR] = _toList(1, "OR"),
-            [Mnemonic.ORNOT] = _toList(1, "ORNOT"),
-            [Mnemonic.ORLD] = _toList(1, "ORLD"),
-            [Mnemonic.OUT] = _toList(1, "OUT"),
+            [Mnemonic.LOAD] = _toInner(1, "LD"),
+            [Mnemonic.LOADNOT] = _toInner(1, "LDNOT"),
+            [Mnemonic.AND] = _toInner(1, "AND"),
+            [Mnemonic.ANDNOT] = _toInner(1, "ANDNOT"),
+            [Mnemonic.ANDLD] = _toInner(1, "ANDLD"),
+            [Mnemonic.OR] = _toInner(1, "OR"),
+            [Mnemonic.ORNOT] = _toInner(1, "ORNOT"),
+            [Mnemonic.ORLD] = _toInner(1, "ORLD"),
+            [Mnemonic.OUT] = _toTerminal(1, "OUT"),
 
             // 옴론은 MPush/MLoad/MPop 을 이용하지 않음
-            [Mnemonic.MPUSH] = _toList(1, "--ERROR:MPUSH"),
-            [Mnemonic.MLOAD] = _toList(1, "--ERROR:MLOAD"),
-            [Mnemonic.MPOP] = _toList(1, "--ERROR:MPOP"),
+            [Mnemonic.MPUSH] = _toTerminal(1, "--ERROR:MPUSH"),
+            [Mnemonic.MLOAD] = _toTerminal(1, "--ERROR:MLOAD"),
+            [Mnemonic.MPOP] = _toTerminal(1, "--ERROR:MPOP"),
 
-            [Mnemonic.CTU] = _toList(2, "CNT"),
-            [Mnemonic.TON] = _toList(1, "TIM"),
-            [Mnemonic.TMR] = _toList(2, "TTIM(087)"),
-            [Mnemonic.KEEP] = _toList(2, "KEEP(011)"),
-            [Mnemonic.SFT] = _toList(3, "SFT(010)"),
-            [Mnemonic.CMP] = _toList(1, "CMP(020)"),
-            [Mnemonic.ADD] = _toList(1, "+(400)"),
-            [Mnemonic.SUB] = _toList(1, "-(410)"),
-            [Mnemonic.MUL] = _toList(1, "*(420)"),
-            [Mnemonic.DIV] = _toList(1, "/(430)"),
+            [Mnemonic.CTU] = _toTerminal(2, "CNT"),
+            [Mnemonic.TON] = _toTerminal(1, "TIM"),
+            [Mnemonic.TMR] = _toTerminal(2, "TTIM(087)"),
+            [Mnemonic.KEEP] = _toInner(2, "KEEP(011)"),
+            [Mnemonic.SFT] = _toTerminal(3, "SFT(010)"),
+            [Mnemonic.CMP] = _toTerminal(1, "CMP(020)"),
+            [Mnemonic.ADD] = _toInner(1, "+(400)"),
+            [Mnemonic.SUB] = _toInner(1, "-(410)"),
+            [Mnemonic.MUL] = _toInner(1, "*(420)"),
+            [Mnemonic.DIV] = _toInner(1, "/(430)"),
 
-            [Mnemonic.BCD] = _toList(1, "BCD(024)"),
-            [Mnemonic.BIN] = _toList(1, "BIN(023)"),
+            [Mnemonic.BCD] = _toTerminal(1, "BCD(024)"),
+            [Mnemonic.BIN] = _toTerminal(1, "BIN(023)"),
 
-            [Mnemonic.CPS] = _toList(1, "CPS(114)"),
-            [Mnemonic.ANDEQ] = _toList(1, "AND=(300)"),
-            [Mnemonic.OREQ] = _toList(1, "OR=(300)"),
-            [Mnemonic.ANDLESSTHAN] = _toList(1, "AND<(310)"),
-            [Mnemonic.ANDGREATERTHAN] = _toList(1, "AND>(320)"),
+            [Mnemonic.CPS] = _toInner(1, "CPS(114)"),
+            [Mnemonic.ANDEQ] = _toInner(1, "AND=(300)"),
+            [Mnemonic.OREQ] = _toInner(1, "OR=(300)"),
+            [Mnemonic.ANDLESSTHAN] = _toInner(1, "AND<(310)"),
+            [Mnemonic.ANDGREATERTHAN] = _toInner(1, "AND>(320)"),
 
 
-            [Mnemonic.MOVE] = _toList(1, "MOV(021)"),
-            [Mnemonic.RSTB] = _toList(1, "RSTB(533)"),
+            [Mnemonic.MOVE] = _toTerminal(1, "MOV(021)"),
+            [Mnemonic.RSTB] = _toTerminal(1, "RSTB(533)"),
 
-            [Mnemonic.CONCATSTRING] = _toList(1, "+$(656)"),
+            [Mnemonic.CONCATSTRING] = _toInner(1, "+$(656)"),
 
-            [Mnemonic.NOP] = _toList(1, "NOP(000)"),
-            [Mnemonic.END] = _toList(1, "END(001)"),
+            [Mnemonic.NOP] = _toTerminal(1, "NOP(000)"),
+            [Mnemonic.END] = _toTerminal(1, "END(001)"),
 
-            [Mnemonic.RUNG_COMMENT] = _toList(0, "'"),
+            [Mnemonic.RUNG_COMMENT] = _toTerminal(0, "'"),
 
         };
     }
---
+++ PLCConvertor/Forms/FormEditPreferences.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PLCConvertor.Forms
+{
+    partial class FormEditPreferences
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.checkEditSplitBySection = new DevExpress.XtraEditors.CheckEdit();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.checkEditForceSplitRung = new DevExpress.XtraEditors.CheckEdit();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEditSplitBySection.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEditForceSplitRung.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.checkEditForceSplitRung);
+            this.layoutControl1.Controls.Add(this.checkEditSplitBySection);
+            this.layoutControl1.Location = new System.Drawing.Point(12, 12);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(351, 198);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.emptySpaceItem1,
+            this.layoutControlItem2});
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(351, 198);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // checkEditSplitBySection
+            // 
+            this.checkEditSplitBySection.EditValue = true;
+            this.checkEditSplitBySection.Location = new System.Drawing.Point(12, 12);
+            this.checkEditSplitBySection.Name = "checkEditSplitBySection";
+            this.checkEditSplitBySection.Properties.Caption = "Split by section";
+            this.checkEditSplitBySection.Size = new System.Drawing.Size(327, 28);
+            this.checkEditSplitBySection.StyleController = this.layoutControl1;
+            this.checkEditSplitBySection.TabIndex = 4;
+            this.checkEditSplitBySection.ToolTip = "Force split CXP section into Xg5k program.";
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.checkEditSplitBySection;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(331, 32);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 64);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(331, 114);
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // checkEditForceSplitRung
+            // 
+            this.checkEditForceSplitRung.EditValue = true;
+            this.checkEditForceSplitRung.Location = new System.Drawing.Point(12, 44);
+            this.checkEditForceSplitRung.Name = "checkEditForceSplitRung";
+            this.checkEditForceSplitRung.Properties.Caption = "Force split rung";
+            this.checkEditForceSplitRung.Size = new System.Drawing.Size(327, 28);
+            this.checkEditForceSplitRung.StyleController = this.layoutControl1;
+            this.checkEditForceSplitRung.TabIndex = 5;
+            this.checkEditForceSplitRung.ToolTip = "Force split rung by rung for possible error.";
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.checkEditForceSplitRung;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 32);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(331, 32);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // FormEditPreferences
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 229);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "FormEditPreferences";
+            this.Text = "FormEditPreferences";
+            this.Load += new System.EventHandler(this.FormEditPreferences_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEditSplitBySection.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEditForceSplitRung.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraEditors.CheckEdit checkEditForceSplitRung;
+        private DevExpress.XtraEditors.CheckEdit checkEditSplitBySection;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+    }
+}
