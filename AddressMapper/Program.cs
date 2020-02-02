6fb81d4aabda4e9d8776882d8903b6c166e67928
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2020-02-02 20:50:35 +0000

    - LSIS 옴론 PLC 변환기   * Address Mapping tools 작업 중.

---
+++ AddressMapper/Program.cs
@@ -0,0 +1,75 @@
+﻿using Dsu.Common.Utilities;
+using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressMapper
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var logger = LogManager.GetLogger("AppFx");
+            FormRibonApp.Logger = logger;
+
+            InstallUnhandledExceptionHandler();
+
+
+            // Configure log4net
+            XmlConfigurator.Configure(new FileInfo(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile));
+
+
+            var root = ((log4net.Repository.Hierarchy.Hierarchy)log4net.LogManager.GetRepository()).Root;
+            var form = new FormRibonApp();
+            root.AddAppender(form);
+
+
+            Application.Run(form);
+
+            void InstallUnhandledExceptionHandler()
+            {
+                UnhandledExceptionEventHandler unhandled = (s, e) => {
+                    System.Media.SystemSounds.Beep.Play();
+                    var msg = $"XXXUnhandled Exception: {e} {e.ExceptionObject}";
+                    //DxMessageBox.Error(msg);
+                    logger.Error(msg);
+                };
+
+                ThreadExceptionEventHandler threadedUnhandled = (s, e) => {
+                    System.Media.SystemSounds.Beep.Play();
+                    var msg = $"Threaded unhandled Exception: {e.Exception}";
+                    //DxMessageBox.Error(msg);
+                    logger.Error(msg);
+                };
+
+                EventHandler<UnobservedTaskExceptionEventArgs> onUnobservedTaskException = (s, e) =>
+                {
+                    System.Media.SystemSounds.Beep.Play();
+                    var msg = $"Unobserved task Exception: {e.Exception}";
+                    //DxMessageBox.Error(msg);
+                    logger.Error(msg);
+
+                    // set observerd 처리 안하면 re-throw 됨
+                    e.SetObserved();
+                };
+
+                EmException.InstallUnhandledExceptionHandler(unhandled, threadedUnhandled, onUnobservedTaskException);
+            }
+
+
+        }
+    }
+}
---
+++ AddressMapper/UcPanelLog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using DevExpress.XtraEditors.Controls;
+//using Images = Dsu.Common.Resources.Images;
+
+namespace AddressMapper
+{
+    public partial class UcPanelLog : UserControl
+    {
+        public ListBoxItemCollection Items => listBoxControlOutput.Items;
+        public int SelectedIndex { get => listBoxControlOutput.SelectedIndex; set => listBoxControlOutput.SelectedIndex = value; }
+
+        public UcPanelLog()
+        {
+            InitializeComponent();
+        }
+
+        private void UcPaneLog_Load(object sender, EventArgs args)
+        {
+            listBoxControlOutput.Dock = DockStyle.Fill;
+            listBoxControlOutput.AllowHtmlDraw = DevExpress.Utils.DefaultBoolean.True;
+
+            var items = listBoxControlOutput.ContextMenuStrip.Items;
+            items.Add(new ToolStripMenuItem("Clear", Images.Clear, (o, a) =>
+            {
+                listBoxControlOutput.Items.Clear();
+                listBoxControlOutput.SelectedIndex = 0;
+            }));
+
+            items.Add(new ToolStripMenuItem("Copy all", Images.Copy, (o, a) =>
+            {
+                var strings =
+                    from n in Enumerable.Range(0, listBoxControlOutput.Items.Count)
+                    let t = listBoxControlOutput.Items[n].ToString()
+                    select Regex.Replace(t, "<.*?>", "")
+                    ;
+
+                var text = String.Join("\r\n", strings);
+                Clipboard.SetText(text);
+            }));
+
+            items.Add(new ToolStripMenuItem("Copy selected", Images.Copy, (o, a) =>
+            {
+                var strings =
+                    from item in listBoxControlOutput.SelectedItems
+                    let str = item.ToString()
+                    select Regex.Replace(str, "<.*?>", "")
+                ;
+
+                var text = String.Join("\r\n", strings);
+                Clipboard.SetText(text);
+            }));
+
+            listBoxControlOutput.MouseClick += (s, e) => {
+                if (e.Button == MouseButtons.Right)
+                    listBoxControlOutput.ContextMenuStrip.Show();
+            };
+
+        }
+    }
+}
