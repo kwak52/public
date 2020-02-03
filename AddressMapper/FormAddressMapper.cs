6dd9259caed8c1f526beaa9fa766ca45d0091387
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2020-02-03 09:12:33 +0000

    - LSIS 옴론 PLC 변환기   * Address Mapping tools 작업 중.

--- AddressMapper/FormAddressMapper.cs
+++ AddressMapper/FormAddressMapper.cs
@@ -11,6 +11,8 @@ using DevExpress.XtraBars;
 using log4net.Appender;
 using System.Configuration;
 using DevExpress.XtraBars.Docking;
+using DevExpress.XtraEditors;
+using System.Diagnostics;
 
 namespace AddressMapper
 {
@@ -34,8 +36,17 @@ namespace AddressMapper
                     dockPanel.Hide();
             };
             action.Update += (s, e) => checkItem.Checked = dockPanel.Visibility == DockVisibility.Visible;
-
         }
+
+        //NumericRangeControlClient numericRangeControlClient1;
+        //void CreateNumericRangeClient()
+        //{
+        //    this.numericRangeControlClient1 = new DevExpress.XtraEditors.NumericRangeControlClient();
+        //    //customRangeControl1.Client = this.numericRangeControlClient1;
+        //    this.numericRangeControlClient1.Maximum = 10000;
+        //    //this.numericRangeControlClient1.RangeControl = this.customRangeControl1;
+        //    this.numericRangeControlClient1.RulerDelta = 16;
+        //}
         public FormAddressMapper()
         {
             InitializeComponent();
@@ -44,10 +55,100 @@ namespace AddressMapper
         private void FormAddressMapper_Load(object sender, EventArgs args)
         {
             Logger.Info("FormAddressMapper launched.");
-            WireDockPanelVisibility(action1, dockPanelMain, barCheckItemShowMain);
-            WireDockPanelVisibility(action1, dockPanelLog, barCheckItemShowLog);
-            WireDockPanelVisibility(action1, dockPanelSource, barCheckItemSource);
-            WireDockPanelVisibility(action1, dockPanelTarget, barCheckItemTarget);            
+            //WireDockPanelVisibility(action1, dockPanelMain, barCheckItemShowMain);
+            //WireDockPanelVisibility(action1, dockPanelLog, barCheckItemShowLog);
+            //WireDockPanelVisibility(action1, dockPanelSource, barCheckItemSource);
+            //WireDockPanelVisibility(action1, dockPanelTarget, barCheckItemTarget);
+
+
+            rangeControl1.RangeChanging += (s, e) =>
+            {
+                var r = e.Range;
+                Trace.WriteLine($"Range changing : {r.Minimum} ~ {r.Maximum}");
+            };
+            rangeControl1.RangeChanged += (s, e) =>
+            {
+                var r = e.Range;
+                Trace.WriteLine($"Range changed. : {r.Minimum} ~ {r.Maximum}");
+            };
+
+            //rangeControlNormal.RangeChanging += (s, e) => Trace.WriteLine("Range changing...");
+            rangeControlNormal.RangeChanged += (s, e) =>
+            {
+                var r = rangeControlNormal.SelectedRange;
+                Trace.WriteLine($"Range changed : {r.Minimum} ~ {r.Maximum}");
+            };
         }
+    }
+
+    /// <summary>
+    /// Custom range control : https://www.devexpress.com/Support/Center/Question/Details/T397835/rangecontrol-where-is-the-rangechanging-event
+    /// </summary>
+    public class CustomRangeControl : RangeControl
+    {
+        public delegate void RangeChangingEventHandler(object sender, RangeControlRangeEventArgs e);
+        public event RangeChangingEventHandler RangeChanging;
+
+        public CustomRangeControl() : base() { }
+        protected override RangeControlHandler CreateHandler()
+        {
+            return new CustomRangeControlHandler(this);
+        }
+        internal void RaiseChangingEvent()
+        {
+            //var r = SelectedRange;
+            //Trace.WriteLine($"Range changing event : {r.Maximum} ~ {r.Minimum}");
+            if (RangeChanging != null)
+            {
+                var d1 = Client.GetValue(RangeViewInfo.RangeMinimum);
+                var d2 = Client.GetValue(RangeViewInfo.RangeMaximum);
+                var range = new RangeControlRange(d1, d2);
+                
+                Trace.WriteLine($"{d1} -- {d2}");
+                RangeChanging(this, new RangeControlRangeEventArgs() { Range = range });
+            }
+        }
+    }
+    public class CustomRangeControlHandler : RangeControlHandler
+    {
+        public CustomRangeControlHandler(RangeControl rangeControl) : base(rangeControl) { }
+        void RaiseRangeChangeEvent(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (ViewInfo.HotInfo.HitTest == RangeControlHitTest.MinRangeThumb
+                    || ViewInfo.HotInfo.HitTest == RangeControlHitTest.MaxRangeThumb
+                    || ViewInfo.HotInfo.HitTest == RangeControlHitTest.LeftScaleThumb
+                    || ViewInfo.HotInfo.HitTest == RangeControlHitTest.RightScaleThumb
+
+                    || ViewInfo.PressedInfo.HitTest == RangeControlHitTest.MinRangeThumb
+                    || ViewInfo.PressedInfo.HitTest == RangeControlHitTest.MaxRangeThumb
+                    || ViewInfo.PressedInfo.HitTest == RangeControlHitTest.LeftScaleThumb
+                    || ViewInfo.PressedInfo.HitTest == RangeControlHitTest.RightScaleThumb
+                    )
+                {
+                    (RangeControl as CustomRangeControl).RaiseChangingEvent();
+                }
+            }
+        }
+        public override void OnMouseMove(MouseEventArgs e)
+        {
+            //Trace.Write("M");
+            base.OnMouseMove(e);
+            RaiseRangeChangeEvent(e);
+        }
+        public override void OnMouseUp(MouseEventArgs e)
+        {
+            //Trace.Write("U");
+            base.OnMouseUp(e);
+            RaiseRangeChangeEvent(e);
+        }
+        public override void OnMouseDown(MouseEventArgs e)
+        {
+            //Trace.Write("D");
+            base.OnMouseDown(e);
+            RaiseRangeChangeEvent(e);
+        }
+
     }
 }
