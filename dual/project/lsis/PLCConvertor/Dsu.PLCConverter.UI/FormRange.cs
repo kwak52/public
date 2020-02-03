26ad459d812546a1cfa55977afa8730706b5f87e
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2020-02-03 23:05:36 +0000

    - LSIS 옴론 PLC 변환기   * Address Mapping tools 작업 중.

---
+++ dual/project/lsis/PLCConvertor/Dsu.PLCConverter.UI/FormRange.cs
@@ -0,0 +1,48 @@
+﻿using Dsu.Common.Utilities.ExtensionMethods;
+using Dsu.PLCConverter.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dsu.PLCConverter.UI
+{
+    public partial class FormRange : Form
+    {
+        UcMemoryBar _memBar;
+        public FormRange(UcMemoryBar memBar)
+        {
+            InitializeComponent();
+            _memBar = memBar;
+        }
+
+        private void FormRange_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            _memBar.SelectedRange.Minimum = int.Parse(textEditStart.Text);
+            _memBar.SelectedRange.Maximum = int.Parse(textEditEnd.Text);
+        }
+
+        private void textEditStart_Validating(object sender, CancelEventArgs e)
+        {
+            var min = textEditStart.Text.TryParseInt();
+            var max = textEditEnd.Text.TryParseInt();
+            if (    min == null || max == null
+                || !min.Value.InClosedRange(0, _memBar.Maximum)
+                || !max.Value.InClosedRange(min.Value, _memBar.Maximum))
+            {
+                e.Cancel = true;
+            }
+            Console.WriteLine("");
+        }
+    }
+}
