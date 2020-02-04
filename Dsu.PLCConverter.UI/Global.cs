c0e0203ed626c6c51101b8eb7bab39e958ed76c2
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2020-02-04 04:25:27 +0000

    - LSIS 옴론 PLC 변환기   * Address Mapping tools 작업 중.

---
+++ Dsu.PLCConverter.UI/Global.cs
@@ -0,0 +1,14 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsu.PLCConverter.UI
+{
+    public class Global
+    {
+        public static ILog Logger { get; set; }
+    }
+}
