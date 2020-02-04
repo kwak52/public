f2d7dcfefdd7e1bc4fb44b49353e0373854bd1f9
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2020-02-04 04:26:35 +0000

    - LSIS 옴론 PLC 변환기   * Address Mapping tools 작업 중.

---
+++ Dsu.PLCConverter.UI/AddressMapperLogics/Types.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsu.PLCConverter.UI.AddressMapperLogics
+{
+    public interface IMemorySection { }
+
+    public class RangedMemory
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public RangedMemory(int start, int end)
+        {
+            Start = start;
+            End = end;
+        }
+        [JsonConstructor] protected RangedMemory() {}
+    }
+
+    public class NamedRangedMemory : RangedMemory
+    {
+        /// <summary>
+        /// "PIO", "M", "T", ...
+        /// </summary>
+        public string Name { get; set; }
+        public NamedRangedMemory(string name, int start, int end)
+            : base(start, end)
+        {
+            Name = name;
+        }
+        [JsonConstructor] protected NamedRangedMemory() { }
+    }
+
+
+    public class MemorySection : NamedRangedMemory, IMemorySection
+    {
+        public MemorySection(string name, int start, int end)
+            : base(name, start, end)
+        {
+        }
+        [JsonConstructor] protected MemorySection() {}
+
+        [JsonIgnore]
+        public List<MemoryRange> MemoryRanges = new List<MemoryRange>();
+    }
+
+    /// <summary>
+    /// 옴론 메모리 종류(W, H, D 등)에 따른 정보
+    /// </summary>
+    public class OmronMemorySection : MemorySection
+    {
+        /// <summary>
+        /// Mapping 가능한 XG5000의 memory 영역 이름
+        /// </summary>
+        public List<string> MappablesNames = new List<string>();
+        public OmronMemorySection(string name, int start, int end, IEnumerable<string> mappableNames)
+            : base(name, start, end)
+        {
+            MappablesNames = mappableNames.ToList();
+        }
+    }
+
+    public class MemoryRange : RangedMemory
+    {
+        public MemorySection Parent { get; set; }
+        public MemoryRange(int start, int end, MemorySection parent)
+            : base(start, end)
+        {
+            Parent = parent;
+        }
+    }
+
+    public class Xg5kMemorySection : MemorySection
+    {
+        public Xg5kMemorySection(string name, int start, int end)
+            : base(name, start, end)
+        {
+        }
+        [JsonConstructor] Xg5kMemorySection() {}
+    }
+
+    /// <summary>
+    /// 옴론 모든 PLC type 별 정의
+    /// </summary>
+    public class OmronPLC
+    {
+        List<OmronMemorySection> _memories = new List<OmronMemorySection>();
+        public string PLCType { get; set; }
+        public OmronMemorySection[] Memories => _memories.ToArray();
+        public OmronPLC(string plcType, IEnumerable<OmronMemorySection> memSections)
+        {
+            PLCType = plcType;
+            _memories = memSections.ToList();
+        }
+
+        [JsonConstructor] OmronPLC() {}
+    }
+
+    /// <summary>
+    /// 산전 모든 PLC type 별 정의
+    /// </summary>
+    public class Xg5kPLC
+    {
+        List<Xg5kMemorySection> _memories = new List<Xg5kMemorySection>();
+        public string PLCType { get; set; }
+        public Xg5kMemorySection[] Memories => _memories.ToArray();
+        public Xg5kPLC(string plcType, IEnumerable<Xg5kMemorySection> memSections)
+        {
+            PLCType = plcType;
+            _memories = memSections.ToList();
+        }
+
+        [JsonConstructor] Xg5kPLC() {}
+    }
+
+
+    /// <summary>
+    /// 옴론 및 산전 모든 PLC type 별 정의
+    /// </summary>
+    public class PLCs
+    {
+        List<OmronPLC> _omronPLCs = new List<OmronPLC>();
+        List<Xg5kPLC> _xg5000PLCs = new List<Xg5kPLC>();
+
+        public OmronPLC[] OmronPLCs => _omronPLCs.ToArray();
+        public Xg5kPLC[] XG5000PLCs => _xg5000PLCs.ToArray();
+        public PLCs(IEnumerable<OmronPLC> omronPLCs, IEnumerable<Xg5kPLC> xg5kPLCs)
+        {
+            _omronPLCs = omronPLCs.ToList();
+            _xg5000PLCs = xg5kPLCs.ToList();
+        }
+        [JsonConstructor] PLCs() {}
+    }
+}
