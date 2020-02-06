fd165c994dba6e93546d39c45e465ea3502b9fe2
dsk <dsk@7f356291-ef72-4cd4-bb90-48bb8829b812>
2020-02-06 21:21:59 +0000

    - LSIS 옴론 PLC 변환기   * Address Mapping tools 작업 중.

--- dual/project/lsis/PLCConvertor/Dsu.PLCConverter.UI/FormRange.Designer.cs
+++ dual/project/lsis/PLCConvertor/Dsu.PLCConverter.UI/FormRange.Designer.cs
@@ -38,6 +38,8 @@
             this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
             this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
             this.btnApply = new DevExpress.XtraEditors.SimpleButton();
+            this.textEditLength = new DevExpress.XtraEditors.TextEdit();
+            this.layoutControlItemLength = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.textEditEnd.Properties)).BeginInit();
@@ -48,34 +50,37 @@
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditLength.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItemLength)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
             // 
+            this.layoutControl1.Controls.Add(this.textEditLength);
             this.layoutControl1.Controls.Add(this.textEditEnd);
             this.layoutControl1.Controls.Add(this.textEditStart);
             this.layoutControl1.Controls.Add(this.comboUnit);
             this.layoutControl1.Location = new System.Drawing.Point(12, 12);
             this.layoutControl1.Name = "layoutControl1";
             this.layoutControl1.Root = this.layoutControlGroup1;
-            this.layoutControl1.Size = new System.Drawing.Size(250, 125);
+            this.layoutControl1.Size = new System.Drawing.Size(224, 188);
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
             // textEditEnd
             // 
-            this.textEditEnd.Location = new System.Drawing.Point(41, 80);
+            this.textEditEnd.Location = new System.Drawing.Point(41, 114);
             this.textEditEnd.Name = "textEditEnd";
-            this.textEditEnd.Size = new System.Drawing.Size(183, 30);
+            this.textEditEnd.Size = new System.Drawing.Size(159, 30);
             this.textEditEnd.StyleController = this.layoutControl1;
             this.textEditEnd.TabIndex = 6;
             this.textEditEnd.Validating += new System.ComponentModel.CancelEventHandler(this.textEditRange_Validating);
             // 
             // textEditStart
             // 
-            this.textEditStart.Location = new System.Drawing.Point(41, 46);
+            this.textEditStart.Location = new System.Drawing.Point(41, 80);
             this.textEditStart.Name = "textEditStart";
-            this.textEditStart.Size = new System.Drawing.Size(183, 30);
+            this.textEditStart.Size = new System.Drawing.Size(159, 30);
             this.textEditStart.StyleController = this.layoutControl1;
             this.textEditStart.TabIndex = 5;
             this.textEditStart.Validating += new System.ComponentModel.CancelEventHandler(this.textEditRange_Validating);
@@ -86,7 +91,7 @@
             this.comboUnit.Name = "comboUnit";
             this.comboUnit.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.comboUnit.Size = new System.Drawing.Size(197, 30);
+            this.comboUnit.Size = new System.Drawing.Size(171, 30);
             this.comboUnit.StyleController = this.layoutControl1;
             this.comboUnit.TabIndex = 4;
             // 
@@ -98,9 +103,10 @@
             this.layoutControlItem1,
             this.emptySpaceItem1,
             this.layoutControlItem2,
-            this.layoutControlItem3});
+            this.layoutControlItem3,
+            this.layoutControlItemLength});
             this.layoutControlGroup1.Name = "layoutControlGroup1";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(250, 125);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(224, 188);
             this.layoutControlGroup1.TextVisible = false;
             // 
             // layoutControlItem1
@@ -108,50 +114,68 @@
             this.layoutControlItem1.Control = this.comboUnit;
             this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(230, 34);
+            this.layoutControlItem1.Size = new System.Drawing.Size(204, 34);
             this.layoutControlItem1.Text = "단위";
             this.layoutControlItem1.TextSize = new System.Drawing.Size(26, 22);
             // 
             // emptySpaceItem1
             // 
             this.emptySpaceItem1.AllowHotTrack = false;
-            this.emptySpaceItem1.Location = new System.Drawing.Point(216, 34);
+            this.emptySpaceItem1.Location = new System.Drawing.Point(192, 68);
             this.emptySpaceItem1.Name = "emptySpaceItem1";
-            this.emptySpaceItem1.Size = new System.Drawing.Size(14, 71);
+            this.emptySpaceItem1.Size = new System.Drawing.Size(12, 100);
             this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
             // 
             // layoutControlItem2
             // 
             this.layoutControlItem2.Control = this.textEditStart;
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 34);
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 68);
             this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(216, 34);
+            this.layoutControlItem2.Size = new System.Drawing.Size(192, 34);
             this.layoutControlItem2.Text = "시작";
             this.layoutControlItem2.TextSize = new System.Drawing.Size(26, 22);
             // 
             // layoutControlItem3
             // 
             this.layoutControlItem3.Control = this.textEditEnd;
-            this.layoutControlItem3.Location = new System.Drawing.Point(0, 68);
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 102);
             this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.Size = new System.Drawing.Size(216, 37);
+            this.layoutControlItem3.Size = new System.Drawing.Size(192, 66);
             this.layoutControlItem3.Text = "끝";
             this.layoutControlItem3.TextSize = new System.Drawing.Size(26, 22);
             // 
             // btnApply
             // 
-            this.btnApply.Location = new System.Drawing.Point(138, 143);
+            this.btnApply.Location = new System.Drawing.Point(112, 202);
             this.btnApply.Name = "btnApply";
             this.btnApply.Size = new System.Drawing.Size(112, 34);
             this.btnApply.TabIndex = 1;
             this.btnApply.Text = "적용";
             this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
             // 
+            // textEditLength
+            // 
+            this.textEditLength.Enabled = false;
+            this.textEditLength.Location = new System.Drawing.Point(41, 46);
+            this.textEditLength.Name = "textEditLength";
+            this.textEditLength.Size = new System.Drawing.Size(171, 30);
+            this.textEditLength.StyleController = this.layoutControl1;
+            this.textEditLength.TabIndex = 7;
+            // 
+            // layoutControlItemLength
+            // 
+            this.layoutControlItemLength.Control = this.textEditLength;
+            this.layoutControlItemLength.Location = new System.Drawing.Point(0, 34);
+            this.layoutControlItemLength.Name = "layoutControlItemLength";
+            this.layoutControlItemLength.Size = new System.Drawing.Size(204, 34);
+            this.layoutControlItemLength.Text = "길이";
+            this.layoutControlItemLength.TextSize = new System.Drawing.Size(26, 22);
+            // 
             // FormRange
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(270, 191);
+            this.ClientSize = new System.Drawing.Size(240, 248);
             this.Controls.Add(this.btnApply);
             this.Controls.Add(this.layoutControl1);
             this.MaximizeBox = false;
@@ -169,6 +193,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditLength.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItemLength)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -185,5 +211,7 @@
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
         private DevExpress.XtraEditors.SimpleButton btnApply;
+        private DevExpress.XtraEditors.TextEdit textEditLength;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItemLength;
     }
 }
